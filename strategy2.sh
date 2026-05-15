220fee68b5808f9aac3d5b2e296609259381dbe1
maksim7293 <maksim7293@gmail.com>
2026-05-14 21:53:51 -0300

    Update strategy2.sh

--- strategy2.sh
+++ strategy2.sh
@@ -17,7 +17,10 @@ tar -C zapret -xzf zapret2.tar.gz
 cd zapret/*/
 
 ./install_bin.sh
-SECURE_DNS=1 FWTYPE=iptables SKIP_TPWS=1 ./blockcheck2.sh
+SECURE_DNS=1 FWTYPE=iptables SKIP_TPWS=1 NOTEST_HOSTFAKE_HTTP=1 NOTEST_HOSTFAKE_HTTPS=1 NOTEST_FAKE_MULTI_HTTP=1 NOTEST_FAKE_MULTI_HTTPS=1 NOTEST_FAKE_FAKED_HTTP=1 NOTEST_FAKE_FAKED_HTTPS=1 NOTEST_FAKE_HOSTFAKE_HTTP=1 NOTEST_FAKE_HOSTFAKE_HTTPS=1./blockcheck2.sh
+
+
+
 
 rm -rf "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
 echo -e "* NOTE: nfqws-keenetic is stopped. Start it manually if necessary! \n"
